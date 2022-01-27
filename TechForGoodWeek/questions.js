791d505c0668e7579ad87f4f5fab7a7293556305
suraj6501 <67002146+suraj6501@users.noreply.github.com>
2022-01-27 19:49:15 +0530

    Win-Et Enactus REC TFGW - Submission

---
+++ TechForGoodWeek/questions.js
@@ -0,0 +1,113 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "How many letters are there in the English alphabet?",
+    answer: "26",
+    options: [
+      "24",
+      "27",
+      "26",
+      "25"
+    ]
+  },
+    {
+    numb: 2,
+    question: "How many colors are there in a rainbow?",
+    answer: "7",
+    options: [
+      "6",
+      "7",
+      "8",
+      "9"
+    ]
+  },
+    {
+    numb: 3,
+    question: "How many consonants are there in the English alphabet?",
+    answer: "21",
+    options: [
+      "18",
+      "19",
+      "20",
+      "21"
+    ]
+  },
+    {
+    numb: 4,
+    question: "How many days are there in a Leap year?",
+    answer: "366",
+    options: [
+      "367",
+      "364",
+      "365",
+      "366"
+    ]
+  },
+    {
+    numb: 5,
+    question: "How many continents are there in the world?",
+    answer: "7",
+    options: [
+      "7",
+      "9",
+      "5",
+      "6"
+    ]
+  },
+    {
+    numb: 6,
+    question: "How many years are there in one Millenium?",
+    answer: "1000",
+    options: [
+      "1000",
+      "10000",
+      "100000",
+      "1000000"
+    ]
+  },
+    {
+    numb: 7,
+    question: "The largest ‘Democracy’ in the world?",
+    answer: "India",
+    options: [
+      "USA",
+      "India",
+      "Japan ",
+      "North Korea"
+    ]
+  },
+    {
+    numb: 8,
+    question: "How many cards are there in a complete pack of cards?",
+    answer: "52",
+    options: [
+      "52",
+      "48",
+      "54",
+      "50"
+    ]
+  },
+    {
+    numb: 9,
+    question: "Find out the Adjective of Season : ",
+    answer: "Seasoned",
+    options: [
+      "Seasonal",
+      "Seasoning",
+      "Seasonable",
+      "Seasoned"
+    ]
+  },
+    {
+    numb: 10,
+    question: "What is the 15th letter of the English alphabet?",
+    answer: "O",
+    options: [
+      "F",
+      "I",
+      "M",
+      "O"
+    ]
+  },
+];
